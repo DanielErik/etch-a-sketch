7ad78fefc20fbe17cc1fa5abbd65429d227be027
DanielErik <28838695+DanielErik@users.noreply.github.com>
2025-08-04 01:00:02 +0200

    Added drawing Canvas Size now adjustable

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-const xAxis = 16;
-const yAxis = 16;
+let size = 16;
+let conStyle;
 
 document.addEventListener("DOMContentLoaded", function(){
     gridLoad();
 });
 
+function mouseOver(event){
+    event.target.style.backgroundColor = "grey";
+}
+
+function sizeChange(){
+    let sizeChange = prompt("What Size?");
+    size = sizeChange;    
+    let conSize = (10 * size) + "px";
+
+    let container = document.querySelector('.gridContainer');
+    container.style.height = conSize;
+    container.style.width = conSize;
+    gridLoad();
+}
+
 function gridLoad(){
     const container = document.querySelector('.gridContainer');
-    for(let x = 0; x < xAxis; x++){
-        for(let y = 0; y < yAxis; y++){
+
+    while(container.lastChild){
+        container.removeChild(container.lastChild);
+    }
+    conStyle = getComputedStyle(container);
+
+    for(let x = 0; x < size; x++){
+        for(let y = 0; y < size; y++){
             let div = document.createElement("div");
-                div.className ="pixel";
+            div.id ="pixel";
+            div.addEventListener("mouseover", mouseOver);
+            
             container.appendChild(div);
         }
     }
